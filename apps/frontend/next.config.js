21db711d13148c4375201c54d2096be91b81fef0
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2022-11-10 01:49:22 +0600

    config: styledComponents, reactStrictMode

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -7,6 +7,10 @@ const { withNx } = require('@nrwl/next/plugins/with-nx');
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
