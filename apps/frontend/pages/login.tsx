ac19089f975985010a21c17ada7871999f652638
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2022-11-09 18:05:22 +0600

    feat: login page

---
+++ apps/frontend/pages/login.tsx
@@ -0,0 +1,12 @@
+import Container from '@mui/system/Container/Container';
+import { NextPage } from 'next/types';
+
+const Login: NextPage = () => {
+  return (
+    <Container>
+      <h1>Login</h1>
+    </Container>
+  );
+};
+
+export default Login;
