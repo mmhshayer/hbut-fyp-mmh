2d6112d934fca00abaa09040c4c9d7c222bc3805
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2022-11-15 21:57:53 +0600

    fix: partial: types: any

--- apps/frontend/shared/trim-values.util.ts
+++ apps/frontend/shared/trim-values.util.ts
@@ -1,8 +1,5 @@
 export const trimValues = (values: any) => {
   return Object.fromEntries(
-    Object.entries(values).map(([key, value]) => [
-      key,
-      (value as string).trim(),
-    ])
+    Object.entries(values).map(([key, value]) => [key, (value as any).trim()])
   );
 };
