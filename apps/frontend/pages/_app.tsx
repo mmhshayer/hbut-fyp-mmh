bfaba61e5c4fe24f1415d990cfcdbd62693017bf
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2022-11-30 16:55:59 +0600

    feat: toast css

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import { Defaultlayout } from '../layouts';
 import { ToastContainer } from 'react-toastify';
+ import 'react-toastify/dist/ReactToastify.css';
 
 function App({ Component, pageProps }: AppProps) {
   return (
