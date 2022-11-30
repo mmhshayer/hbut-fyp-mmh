9717ecc1225b455cf61045048b00822a3bf3eed0
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2022-11-30 20:24:53 +0600

    show user company currentCompany info

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,11 +1,21 @@
 import { useUser } from '../features/user';
 
 export function Index() {
-  const { user } = useUser();
+  const { user, companies, currentCompany } = useUser();
   return (
     <>
       <h1>Home</h1>
       {user ? <pre>{JSON.stringify(user, null, 2)}</pre> : <p>Not logged in</p>}
+      {companies ? (
+        <pre>{JSON.stringify(companies, null, 2)}</pre>
+      ) : (
+        <p>No Companies</p>
+      )}
+      {currentCompany ? (
+        <pre>{JSON.stringify(currentCompany, null, 2)}</pre>
+      ) : (
+        <p>No Current Company</p>
+      )}
     </>
   );
 }
