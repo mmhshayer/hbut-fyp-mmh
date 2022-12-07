ef82a0849f97343b01af8152c23aa1521b4a95ca
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2022-12-07 17:07:45 +0600

    updated product card

--- apps/frontend/components/products/card.component.tsx
+++ apps/frontend/components/products/card.component.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, CardMedia, Typography } from '@mui/material';
+import { Card, CardActionArea, CardContent, CardHeader, CardMedia, Collapse, Typography } from '@mui/material';
 import Link from 'next/link';
 import { PageProps } from '../../shared/page.interface';
 
@@ -9,22 +9,24 @@ interface ProductCardProps extends PageProps {
 export default function ProductCard({ sx, product }: ProductCardProps) {
     return (
         <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-                title={<Link href={`${product.company}/${product.name}`} style={{
-                    textDecoration: 'none',
-                    color: 'inherit'
-                }}>{product.name}</Link>}
-                subheader={product.price + ' BDT'}
-            />
-            <CardMedia
-                component="img"
-                height="194"
-                image="/placeholder-400x400.jpg"
-                alt="Paella dish"
-            />
-            <CardContent>
-                <Typography variant="h6">{product.description}</Typography>
-            </CardContent>
+            <CardActionArea href={`${product.company.name}/${product.name}`}>
+                <CardHeader
+                    title={product.name}
+                    subheader={product.price + ' BDT'}
+                />
+                <CardMedia
+                    component="img"
+                    height="194"
+                    image="/placeholder-400x400.jpg"
+                    alt="Paella dish"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                        by {product.company.name}
+                    </Typography>
+                    <Typography variant="h6" noWrap>{product.description}</Typography>
+                </CardContent>
+            </CardActionArea>
         </Card>
     )
 };
