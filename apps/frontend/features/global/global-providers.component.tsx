d00dea517e0ef4173d0604b2ee4af323fee21cf1
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2022-12-10 16:05:21 +0600

    I: react-use-cart

--- apps/frontend/features/global/global-providers.component.tsx
+++ apps/frontend/features/global/global-providers.component.tsx
@@ -3,18 +3,21 @@ import { AuthProvider } from '../auth';
 import { RouteGuard, RouteGuardProvider } from '../router';
 import { ThemeProvider } from '../../theme';
 import { UserProvider } from '../user';
+import { CartProvider } from "react-use-cart";
 
 const GlobalProviders: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <AuthProvider>
-      <UserProvider>
-        <RouteGuardProvider>
-          <ThemeProvider>
-            <RouteGuard>{children}</RouteGuard>
-          </ThemeProvider>
-        </RouteGuardProvider>
-      </UserProvider>
-    </AuthProvider>
+    <CartProvider>
+      <AuthProvider>
+        <UserProvider>
+          <RouteGuardProvider>
+            <ThemeProvider>
+              <RouteGuard>{children}</RouteGuard>
+            </ThemeProvider>
+          </RouteGuardProvider>
+        </UserProvider>
+      </AuthProvider>
+    </CartProvider>
   );
 };
 
