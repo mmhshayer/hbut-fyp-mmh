01fa5914b85d92f1dbc64d0ada9e81abe807ff27
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2022-12-13 15:55:17 +0600

    order from POS

--- apps/frontend/components/pos/pos.component.tsx
+++ apps/frontend/components/pos/pos.component.tsx
@@ -1,7 +1,135 @@
-
+import Box from "@mui/material/Box";
+import useApi from '../../features/api/use-api.hook';
+import { Product } from "../products/product.interface";
+import { useEffect } from 'react';
+import { Button, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import RemoveIcon from '@mui/icons-material/Remove';
+import PageLoader from '../common/page-loader.component';
+import Typography from '@mui/material/Typography';
+import { useCart } from 'react-use-cart';
+import { useRouter } from "next/router";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
 
 export default function Pos() {
+    const router = useRouter();
+    const { emptyCart, addItem, removeItem, items, cartTotal
+    } = useCart()
+
+    const { data: products, loading: productsLoading } = useApi<Product[]>({
+        url: '/products',
+    });
+
+    const { data, loading, callApi } = useApi({
+        url: '/orders',
+        method: 'POST',
+        lazy: true,
+    });
+
+    useEffect(() => {
+        if (router.isReady) {
+            emptyCart();
+        }
+    }, [router])
+
+    const handleCheckout = () => {
+        const checkout = {
+            products: items.map((item) => ({
+                product: item._id,
+                quantity: item.quantity
+            })),
+            total: items.reduce((acc, item) => acc + (item.price * item.quantity), 0),
+        }
+        // callApi(checkout)
+        console.log(checkout)
+    }
+
+    if (!products) return null;
+
     return (
-        <div>POS</div>
+        <PageLoader loading={productsLoading}>
+            <Box sx={{
+                display: 'flex',
+                flexDirection: {
+                    xs: 'column',
+                    md: 'row',
+                },
+                justifyContent: {
+                    xs: 'center',
+                    md: 'space-between',
+                },
+                border: '1px solid black',
+                padding: '10px',
+                borderRadius: '5px',
+                gap: '10px',
+            }}>
+                {products.length > 0 && (
+
+                    <TableContainer component={Paper}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Product</TableCell>
+                                    <TableCell>Price</TableCell>
+                                    <TableCell>Action</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {products.map((product) => (
+                                    <TableRow key={product._id}>
+                                        <TableCell>{product.name}</TableCell>
+                                        <TableCell>{product.price}</TableCell>
+                                        <TableCell>
+                                            <IconButton onClick={() => addItem({
+                                                id: product._id,
+                                                ...product
+                                            })}>
+                                                <AddShoppingCartIcon />
+                                            </IconButton>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+
+                )}
+                {items.length > 0 && (
+                    <TableContainer component={Paper}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Product</TableCell>
+                                    <TableCell>Price</TableCell>
+                                    <TableCell>Quantity</TableCell>
+                                    <TableCell>Action</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {items.map((item) => (
+                                    <TableRow key={item.id}>
+                                        <TableCell>{item.name}</TableCell>
+                                        <TableCell>{item.price}</TableCell>
+                                        <TableCell>{item.quantity}</TableCell>
+                                        <TableCell>
+                                            <IconButton onClick={() => removeItem(item.id)}>
+                                                <RemoveShoppingCartIcon />
+                                            </IconButton>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                                <TableRow>
+                                    <TableCell colSpan={2}>Total</TableCell>
+                                    <TableCell>{cartTotal}</TableCell>
+                                    <TableCell>
+                                        <Button onClick={handleCheckout} variant='contained'>Checkout</Button>
+                                    </TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                )}
+            </Box>
+        </PageLoader>
     )
 }
--- apps/frontend/pages/dashboard/pos.tsx
+++ apps/frontend/pages/dashboard/pos.tsx
@@ -1,17 +1,10 @@
 import Container from '@mui/system/Container';
-import { useUser } from '../../features/user';
 import { NextPage } from 'next';
 import { Pos } from '../../components/pos';
 
 const POS: NextPage = () => {
-    const { user, currentCompany } = useUser()
     return (
         <Container>
-            <div>
-                <h1>Dashboard</h1>
-                <p>user id: {user?._id}</p>
-                <p>Current Company: {currentCompany?.name}</p>
-            </div>
             <Pos />
         </Container>
     );
