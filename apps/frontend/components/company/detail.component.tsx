2220fdb3a6f30a653ec835ffd0e4fbb1e998a8c9
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2022-12-18 17:30:05 +0600

    store page

--- apps/frontend/components/company/detail.component.tsx
+++ apps/frontend/components/company/detail.component.tsx
@@ -2,28 +2,56 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import useApi from '../../features/api/use-api.hook';
 import { PageProps } from '../../shared/page.interface';
-
+import PageLoader from '../common/page-loader.component';
+import { Box, Typography } from '@mui/material';
+import { Company } from '../../features/user';
+import { ProductCard } from '../products';
 
 export default function CompanyDetail({ sx }: PageProps) {
     const router = useRouter();
     const { company } = router.query;
 
-    const { data, loading, loaded, error, callApi } = useApi({
+    const { data: companyInfo } = useApi<Company>({
         url: `/company/${company}`,
-        lazy: true
     });
 
+    const companyProducts = useApi<any[]>({
+        url: `/products/company/${companyInfo?._id}`,
+        lazy: true,
+    })
+
     useEffect(() => {
-        callApi();
-    }, [router.isReady])
+        if (companyInfo?._id) {
+            companyProducts.callApi();
+        }
+    }, [companyInfo?._id])
 
-    console.log(data);
-
-    if (error) {
-        return <div>Error</div>
-    }
 
     return (
-        <div>CompanyDetails</div>
+        <PageLoader loading={false}>
+            <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                <Typography variant="h1" >
+                    {companyInfo?.name}
+                </Typography>
+                <Typography variant="caption">
+                    {companyInfo?.status}
+                </Typography>
+            </Box>
+            <Box sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'center',
+                gap: 2,
+                p: 2,
+            }}>
+                {companyProducts.data?.map((product, index) => <ProductCard key={index} product={product} />)}
+            </Box>
+
+        </PageLoader>
     )
 }
--- apps/frontend/features/user/index.ts
+++ apps/frontend/features/user/index.ts
@@ -1,2 +1,3 @@
 export { default as UserProvider } from './user.provider';
 export { default as useUser } from './use-user.hook';
+export type { default as Company } from './company.interface';
